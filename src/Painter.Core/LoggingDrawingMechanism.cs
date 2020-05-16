e0c1b34506e805ae0f427b92530934ed8a31abaf
Mylov Vasily <zarkaria.dia@gmail.com>
2020-05-17 03:56:56 +0500

    LoggingDrawingMechanism

---
+++ src/Painter.Core/LoggingDrawingMechanism.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Painter.Core
+{
+    public class LoggingDrawingMechanism : IDrawingMechanism
+    {
+        public Task MoveToAsync(Position position)
+        {
+            Console.WriteLine($"Move to {position}");
+            return Task.CompletedTask;
+        }
+
+        public Task UpAsync()
+        {
+            Console.WriteLine($"Up");
+            return Task.CompletedTask;
+        }
+
+        public Task DownAsync()
+        {
+            Console.WriteLine($"Down");
+            return Task.CompletedTask;
+        }
+    }
+}
